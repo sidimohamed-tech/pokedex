193b392caf089f34c10e8a4f6a3684bee4466c08
Sidi <sidimed.sidimed.147@gmail.com>
2024-10-22 16:05:34 +0200

    components.2

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,7 +1,31 @@
 function PokemonCard() {
-    return <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-    <figcaption>Bulbasaur</figcaption>
-    </figure>;
+    let pokemon = pokemonList[0];
+    let imagevalue=pokemon.imgSrc;
+    return (imagevalue ?(<figure><img src={pokemon.imgSrc} alt="Bulbasaur" />
+    <figcaption>{pokemon.name}</figcaption></figure>) : <p>???</p> );
+    // if(pokemon.imgSrc===undefined)
+    // {
+    //     return (<p>???</p>);
+    // }
+    // else
+    // {
+    //     return (<figure><img src={pokemon.imgSrc} alt="Bulbasaur" />
+    // <figcaption>{pokemon.name}</figcaption></figure>);
+    // }
+
   }
+
+
   
   export default PokemonCard;
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
