24d844cb72ba92c1df4334c108d346d43ea2afd5
Sidi <sidimed.sidimed.147@gmail.com>
2024-10-23 12:12:27 +0200

    state1 fin

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import "./App.css";
-
+import { useState } from "react";
 import PokemonCard from "./components/PokemonCard";
 function App({}) {
+  const [pokemonIndex,setPokemonIndex]=useState(0);
+
+  const handleClickback=()=>{
+      setPokemonIndex(pokemonIndex -1);
+
+    }
+    const handleClicknext=()=>{
+      setPokemonIndex(pokemonIndex +1);
+    }
+  
+ 
+ 
   return (
 <div>
+{pokemonIndex > 0 ? <button type="button" onClick={handleClickback}>précedent</button>:<p></p>}
+{pokemonIndex < pokemonList.length - 1 ?<button type="button" onClick={handleClicknext}>Suivant</button>:<p></p>}
 
 
-<PokemonCard pokemon={pokemonList[0]} />
+<PokemonCard pokemon={pokemonList[pokemonIndex]} />
 </div>
 
-  );
+ );
 }
 
 export default App;
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -9,15 +9,7 @@ function PokemonCard({pokemon} : typeOfProps) {
     let imagevalue=pokemon.imgSrc;
     return (imagevalue ?(<figure><img src={pokemon.imgSrc} alt="Bulbasaur" />
     <figcaption>{pokemon.name}</figcaption></figure>) : <p>???</p> );
-    // if(pokemon.imgSrc===undefined)
-    // {
-    //     return (<p>???</p>);
-    // }
-    // else
-    // {
-    //     return (<figure><img src={pokemon.imgSrc} alt="Bulbasaur" />
-    // <figcaption>{pokemon.name}</figcaption></figure>);
-    // }
+    
 
   }
 
